fcd6f58f690a79b74964560452df72831f80d638
Seth <seth@developer.com>
2015-10-07 16:00:44 -0700

    Initial Commit

---
+++ SwiftLog/SwiftLog.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLog.h
+//  SwiftLog
+//
+//  Created by Seth on 10/7/15.
+//  Copyright © 2015 Seth Arnott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLog.
+FOUNDATION_EXPORT double SwiftLogVersionNumber;
+
+//! Project version string for SwiftLog.
+FOUNDATION_EXPORT const unsigned char SwiftLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLog/PublicHeader.h>
+
+
